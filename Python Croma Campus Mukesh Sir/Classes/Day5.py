8ba8cadabaa3dfd04ba1671d2396b477e37d891a
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2026-04-08 01:42:21 +0530

    Create Day5.py

---
+++ Python Croma Campus Mukesh Sir/Classes/Day5.py
@@ -0,0 +1,135 @@
+"""
+Python Tokens(DataType , Variable , Keyword , Operator )
+Data Types
+
+Numerical
+     int(23,43,64,35,-45,75,-765,0)
+     Float(23.24,-353.54,0.0)
+     binary number(0b1001,0b01011,0b100111)
+     octal number 0o165
+     hexadecimal number
+     0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+     0x1AF  ,  0x244
+     Complex Number (a+jb) => a real num , b imaginary number
+     a = 3+8j
+print(a)
+print(a.real)
+print(a.imag)
+
+
+String
+     'A'  ,  'Aman'  , "A"  ,  "Aman"
+
+Boolean
+      True  , False
+      
+Sequence Data Type (LIST , TUPLE)
+    l = [23,45,56,78]   LIST
+    t = (3,56,67,78,89)  TUPLE
+    
+SET
+    s = {234,56,67,89}
+    
+Dictionary
+    d = {1:45,2:67,5:89}  {key:value}
+
+
+Q WAP to add two numbers
+Output:  Sum of a and b is c
+    Sum of 10 and 20 is 30
+    
+
+
+"""
+a = int(input("Enter a number"))
+b = int(input("Enter b number"))
+c = a+b
+print("Sum of",a,"and",b,"is",c)
+print(f"Sum of {a} and {b} is {c}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
