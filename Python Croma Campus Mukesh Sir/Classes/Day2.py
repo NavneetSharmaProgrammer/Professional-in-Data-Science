f76e60a327180ec985f969f1938ea85bc3ca7b81
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2026-04-08 01:42:00 +0530

    Create Day2.py

---
+++ Python Croma Campus Mukesh Sir/Classes/Day2.py
@@ -0,0 +1,56 @@
+'''
+a = 10
+b = 48
+c = a+b
+print(c)
+#Single Line Comment
+"""
+Multiline
+Comments
+Built-in Function
+"""
+print('Hello India')
+print("Hello India")
+print("""Hello India""")
+
+
+input (It is use to ask a value from a user)
+input always returns a string/text value
+
+int(int (integer) used to convert a value into a number
+float => it is use to convert value into numerical (decimals)
+a = int(input("Enter A number"))
+b = int(input("Enter B number"))
+c = a+b
+print("Addition",c)
+a = float(input("Enter A number"))
+b = float(input("Enter B number"))
+c = a+b
+print("Addition",c)
+
+
+print , input , int , float
+
+
+# WAP to calculate gross salary of an employee
+where HRA is 20% and DA si 30% of his basic salary.
+HINT- Gross_Salary = Basic + HRA +DA
+'''
+
+bs = float( input("Enter Basic Salary ; "))
+hra = bs*20/100
+da = bs*30/100
+gs = bs +hra + da
+print(gs)
+
+
+
+
+
+
+
+
+
+
+
+
