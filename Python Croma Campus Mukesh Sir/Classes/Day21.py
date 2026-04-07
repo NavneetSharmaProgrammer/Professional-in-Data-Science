8cba9e9b94b493f3f21281c1264a61016a8f2df7
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2026-04-08 01:42:02 +0530

    Create Day21.py

---
+++ Python Croma Campus Mukesh Sir/Classes/Day21.py
@@ -0,0 +1,53 @@
+"""
+def isPrime(num):
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+
+num = int(input("Enter A Number"))
+if isPrime(num) and num!=1:
+    print("Prime")
+else:
+    print("Not Prime")
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
