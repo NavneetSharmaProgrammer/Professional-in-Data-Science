5b63eb787ed810c2126496676f69a48a700cc90a
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2026-04-19 21:07:29 +0530

    update

---
+++ Python Croma Campus Mukesh Sir/Classes/Day15.py
@@ -0,0 +1,28 @@
+"""
+Tuple :-  Tuple is also a collection and part of sequence
+data type like List of hetregenous element,
+syntex:-
+t = (23,34,45,56,67,78)
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
