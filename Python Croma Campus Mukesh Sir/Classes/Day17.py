f384504a5bbc09f49a131ac2515bc4d9bc644fc5
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2026-04-24 22:26:08 +0530

    Update Day17.py

--- Python Croma Campus Mukesh Sir/Classes/Day17.py
+++ Python Croma Campus Mukesh Sir/Classes/Day17.py
@@ -41,7 +41,6 @@ print(s.items())
 for x in s.items():
     print(x)
 
-'''
 s = {1:34,2:67,4:78,'A':54}
 print(s)
 print(type(s))
@@ -50,6 +49,42 @@ for k,v in s.items():
     print(k,v)
 
 
+    get
+
+s = {1:34,2:67,4:78,'A':54}
+print(s)
+print(s.get(3,"Not Available"))
+print(s.get(4,"Not Available"))
+# print(s[3])   # it will raise an error
+
+
+
+    Update
+
+
+s1 = {1:34,2:67,4:78,'A':54}
+s2 = {6:24,7:46,4:99,9:45}
+
+s1.update(s2)
+print(s1)
+
+
+
+Built_in functions
+    sum ,max, min, len
+
+
+
+d = {1:234,2:345,3:456}
+print(sum(d))
+print(sum(d.values()))
+print(max(d.values()))
+print(min(d.values()))
+print(len(d.values()))
+
+
+ 
+'''
 
 
 
